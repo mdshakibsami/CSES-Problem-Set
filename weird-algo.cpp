e90f3d112b6185d227676d669b59673c473ab26c
mdshakibsami <shakib.sami2.0@gmail.com>
2024-03-29 06:54:00 +0600

    weird algo problem

---
+++ weird-algo.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+     ll n;
+    cin>>n;
+    cout<<n<<" ";
+    while (n!=1)
+    {
+        if(n&1) n= (n*3)+1;
+        else n/=2;
+        cout<<n<<" ";
+    }
+    
+
+}
